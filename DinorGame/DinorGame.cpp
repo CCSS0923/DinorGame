257493de3e569affafd908cbe1259844308f2e41
CCSS0923 <dmltjd4588@gmail.com>
2025-09-07 22:39:19 +0900

    20250907 final commit at 22:39 [ + ] 선인장 배경 추가 공룡 조작 추가
    Prototype ver 0.0.1

--- DinorGame/DinorGame.cpp
+++ DinorGame/DinorGame.cpp
@@ -1,19 +1,28 @@
-﻿#include "framework.h"
+﻿/*
+조작법 : 
+        스페이스바 : 점프
+        아래 방향키 : 빠르게 낙하
+*/
+
+#include "framework.h"
 #include "DinorGame.h"
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+#include <thread>
+#include <atomic>
 
 #define MAX_LOADSTRING 100
-#define TIMER_ID 1
-#define TIMER_INTERVAL 50 // 50ms 간격 (약 20fps)
+#define TIMER_INTERVAL 50 // 20fps
 
-HINSTANCE hInst;                                // 현재 인스턴스
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
 
-// 공룡 도트 그래픽 정의 (1: 픽셀 있음, 0: 없음)
+// 공룡 도트 그래픽 (20x22)
 const int dinoWidth = 20;
 const int dinoHeight = 22;
-const int dinoPixels[dinoHeight][dinoWidth] =
-{
+const int dinoPixels[dinoHeight][dinoWidth] = {
     {0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0},
     {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
     {0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1},
@@ -38,54 +47,151 @@ const int dinoPixels[dinoHeight][dinoWidth] =
     {0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0},
 };
 
+const int cactusWidth = 8;
+const int cactusHeight = 8;
+const int cactusPixels[cactusHeight][cactusWidth] = {
+    {0,1,0,0,0,1,0,0},
+    {0,1,0,0,0,1,0,0},
+    {1,1,1,0,1,1,1,0},
+    {1,1,1,1,1,1,1,0},
+    {0,1,1,0,1,1,0,0},
+    {0,1,0,0,0,1,0,0},
+    {0,1,0,0,0,1,0,0},
+    {0,1,0,0,0,1,0,0},
+};
 
+const int pixelSize = 10;
 
-int dinoPosX = 50;
-int dinoPosY = 100;
+static std::atomic<int> dinoRealX(50);
+static std::atomic<int> dinoScreenX(50);
+static std::atomic<int> cameraXOffset(0);
 
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+static int dx = 15; // 이동속도
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+static std::atomic<int> dinoPosY(200);
+static std::atomic<bool> isJumping(false);
+static std::atomic<float> jumpVelocity(0.0f);
+
+const float gravity = 3.0f;
+const int groundY = 200;
+
+// 아래키 눌림 상태
+static std::atomic<bool> downKeyPressed(false);
+
+struct Cactus {
+    int x;
+    bool active;
+};
+
+const int MAX_CACTUS = 30;
+static Cactus cactusPool[MAX_CACTUS];
+static std::atomic<int> activeCactusCount(0);
+static std::atomic<bool> running(true);
+
+inline int GetRandomDistance() {
+    return 600 + rand() % 301; // 넉넉한 랜덤 간격
+}
+
+inline int GetInactiveCactusIndex() {
+    for (int i = 0; i < MAX_CACTUS; i++)
+        if (!cactusPool[i].active)
+            return i;
+    return -1;
+}
+
+void InitCactuses(int screenWidth) {
+    for (int i = 0; i < MAX_CACTUS; i++) {
+        cactusPool[i].active = false;
+        cactusPool[i].x = 0;
+    }
+    int currentX = screenWidth + cactusWidth * pixelSize + 300;
+    activeCactusCount = 10;
+    for (int i = 0; i < activeCactusCount; i++) {
+        int idx = GetInactiveCactusIndex();
+        if (idx >= 0) {
+            cactusPool[idx].active = true;
+            cactusPool[idx].x = currentX;
+            currentX += GetRandomDistance();
+        }
+    }
+}
+
+void DrawDino(HDC hdc, int posX, int posY, int pixelSize) {
+    for (int y = 0; y < dinoHeight; y++) {
+        for (int x = 0; x < dinoWidth; x++) {
+            if (dinoPixels[y][x] == 1)
+                Rectangle(hdc, posX + x * pixelSize, posY + y * pixelSize, posX + (x + 1) * pixelSize, posY + (y + 1) * pixelSize);
+        }
+    }
+}
+
+void DrawCactus(HDC hdc, int posX, int posY, int pixelSize) {
+    for (int y = 0; y < cactusHeight; y++) {
+        for (int x = 0; x < cactusWidth; x++) {
+            if (cactusPixels[y][x] == 1)
+                Rectangle(hdc, posX + x * pixelSize, posY + y * pixelSize, posX + (x + 1) * pixelSize, posY + (y + 1) * pixelSize);
+        }
+    }
+}
+
+void CactusProcessingThread(HWND hwnd) {
+    int lastX;
+    while (running) {
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+
+        lastX = rect.right + cactusWidth * pixelSize + 600;
+
+        for (int i = 0; i < MAX_CACTUS; i++) {
+            if (cactusPool[i].active) {
+                cactusPool[i].x -= dx;
+
+                if (cactusPool[i].x + cactusWidth * pixelSize < 0) {
+                    cactusPool[i].x = lastX + cactusWidth * pixelSize + 600 + GetRandomDistance();
+                }
+
+                if (cactusPool[i].x > lastX) {
+                    lastX = cactusPool[i].x;
+                }
+            }
+        }
+
+        InvalidateRect(hwnd, nullptr, TRUE);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(TIMER_INTERVAL));
+    }
+}
+
+ATOM MyRegisterClass(HINSTANCE);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int nCmdShow) {
+    UNREFERENCED_PARAMETER(nCmdShow);
 
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_DINORGAME, szWindowClass, MAX_LOADSTRING);
+
     MyRegisterClass(hInstance);
 
-    if (!InitInstance(hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+    if (!InitInstance(hInstance, nCmdShow)) return FALSE;
+
+    SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
 
     MSG msg;
-    // 타이머 설정: 윈도우 생성 후 타이머로 주기적 WM_TIMER 발생
-    SetTimer(NULL, TIMER_ID, TIMER_INTERVAL, NULL);
-
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
+    while (GetMessage(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
     return (int)msg.wParam;
 }
 
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
+ATOM MyRegisterClass(HINSTANCE hInstance) {
+    WNDCLASSEXW wcex = { sizeof(WNDCLASSEX) };
     wcex.style = CS_HREDRAW | CS_VREDRAW;
     wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
+    wcex.cbClsExtra = 0; wcex.cbWndExtra = 0;
     wcex.hInstance = hInstance;
     wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DINORGAME));
     wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
@@ -93,124 +199,145 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_DINORGAME);
     wcex.lpszClassName = szWindowClass;
     wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
     return RegisterClassExW(&wcex);
 }
 
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
     hInst = hInstance;
+    HWND hwnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, 640, 580, nullptr, nullptr, hInstance, nullptr);
+    if (!hwnd) return FALSE;
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, 0, 640, 480, nullptr, nullptr, hInstance, nullptr);
+    RECT rect;
+    GetClientRect(hwnd, &rect);
+    srand((unsigned int)time(NULL));
 
-    if (!hWnd)
-    {
-        return FALSE;
-    }
+    InitCactuses(rect.right);
 
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
-
-    // 타이머 설정: 창 핸들을 타이머에 연결
-    SetTimer(hWnd, TIMER_ID, TIMER_INTERVAL, NULL);
+    std::thread cactusThread(CactusProcessingThread, hwnd);
+    cactusThread.detach();
 
     return TRUE;
 }
 
-void DrawDino(HDC hdc, int posX, int posY, int pixelSize)
-{
-    for (int y = 0; y < dinoHeight; ++y)
-    {
-        for (int x = 0; x < dinoWidth; ++x)
-        {
-            if (dinoPixels[y][x] == 1)
-            {
-                Rectangle(hdc,
-                    posX + x * pixelSize,
-                    posY + y * pixelSize,
-                    posX + (x + 1) * pixelSize,
-                    posY + (y + 1) * pixelSize);
-            }
-        }
-    }
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    static int dx = 5; // 공룡의 x방향 이동 속도
-
-    switch (message)
-    {
-    case WM_COMMAND:
-    {
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_COMMAND: {
         int wmId = LOWORD(wParam);
-        switch (wmId)
-        {
+        switch (wmId) {
         case IDM_ABOUT:
-            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hwnd, About);
             break;
         case IDM_EXIT:
-            DestroyWindow(hWnd);
+            running.store(false);
+            DestroyWindow(hwnd);
             break;
         default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
+            return DefWindowProc(hwnd, message, wParam, lParam);
         }
-    }
-    break;
-    case WM_PAINT:
-    {
+    } break;
+    case WM_PAINT: {
         PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
+        HDC hdc = BeginPaint(hwnd, &ps);
+        RECT rect;
+        GetClientRect(hwnd, &rect);
 
-        // 공룡 도트 그래픽 그리기
-        DrawDino(hdc, dinoPosX, dinoPosY, 10);
+        const int floorY = 420;
 
-        EndPaint(hWnd, &ps);
-    }
-    break;
+        HBRUSH brush = CreateSolidBrush(RGB(230, 230, 230));
+        RECT marginRect = { 0, floorY + 1, rect.right, rect.bottom };
+        FillRect(hdc, &marginRect, brush);
+        DeleteObject(brush);
 
-    case WM_TIMER:
-        if (wParam == TIMER_ID)
-        {
-            // 공룡 좌표 이동 (간단한 애니메이션)
-            dinoPosX += dx;
+        MoveToEx(hdc, 0, floorY, nullptr);
+        LineTo(hdc, rect.right, floorY);
 
-            // 윈도우 클라이언트 영역 크기 얻기
-            RECT rect;
-            GetClientRect(hWnd, &rect);
+        for (int i = 0; i < MAX_CACTUS; i++) {
+            if (cactusPool[i].active) {
+                DrawCactus(hdc, cactusPool[i].x - cameraXOffset.load(), floorY - pixelSize * cactusHeight, pixelSize);
+            }
+        }
 
-            // 화면 밖으로 나가지 않도록 방향 반전
-            if (dinoPosX + dinoWidth * 10 > rect.right || dinoPosX < 0)
-            {
-                dx = -dx;
+        int centerX = rect.right / 2;
+        int fixedDinoX = centerX - 100;
+
+        if (dinoRealX.load() < fixedDinoX) {
+            int current = dinoRealX.load();
+            int step = dx * 3; // 3배 빠른 중앙 이동
+            int nextPos = current + step;
+            if (nextPos > fixedDinoX) nextPos = fixedDinoX;
+            dinoRealX.store(nextPos);
+            dinoScreenX.store(nextPos);
+            cameraXOffset.store(0);
+        }
+        else {
+            dinoScreenX.store(fixedDinoX);
+            cameraXOffset.store(dinoRealX.load() - fixedDinoX);
+        }
+
+        if (isJumping.load()) {
+            int oldPos = dinoPosY.load();
+            float oldVel = jumpVelocity.load();
+
+            // 아래키 누름에 따른 가속 관리
+            bool downPressed = downKeyPressed.load();
+
+            int fallStep = static_cast<int>(oldVel);
+            // 아래방향키 눌렀으면 기본 낙하 속도의 5배로 이동
+            if (downPressed && fallStep > 0) {
+                fallStep *= 5;
             }
 
-            InvalidateRect(hWnd, NULL, TRUE); // 화면 갱신 요청 (WM_PAINT 호출 유발)
+            dinoPosY.store(oldPos + fallStep);
+            // 가속도 적용
+            jumpVelocity.store(oldVel + gravity);
+
+            if (dinoPosY.load() >= groundY) {
+                dinoPosY.store(groundY);
+                isJumping.store(false);
+                jumpVelocity.store(0.0f);
+                downKeyPressed.store(false); // 착지 후 아래키 상태 초기화
+            }
+        }
+
+        DrawDino(hdc, dinoScreenX.load(), dinoPosY.load(), pixelSize);
+        EndPaint(hwnd, &ps);
+        break;
+    }
+    case WM_KEYDOWN:
+        if (wParam == VK_SPACE && !isJumping.load()) {
+            isJumping.store(true);
+            jumpVelocity.store(-35.0f);
+        }
+        else if (wParam == VK_DOWN) {
+            if (!downKeyPressed.load()) {
+                downKeyPressed.store(true);
+            }
         }
         break;
-
+    case WM_KEYUP:
+        if (wParam == VK_DOWN) {
+            downKeyPressed.store(false);
+        }
+        break;
     case WM_DESTROY:
-        KillTimer(hWnd, TIMER_ID);
+        running.store(false);
         PostQuitMessage(0);
         break;
-
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, message, wParam, lParam);
     }
     return 0;
 }
 
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
+INT_PTR CALLBACK About(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
     case WM_INITDIALOG:
         return (INT_PTR)TRUE;
     case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
             EndDialog(hDlg, LOWORD(wParam));
             return (INT_PTR)TRUE;
         }
