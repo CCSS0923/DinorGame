e37f4ddf2ffe8499f602b29ece93db5ab4854b28
CCSS0923 <dmltjd4588@gmail.com>
2025-09-07 16:22:04 +0900

    [ + ] Dinor

--- DinorGame/DinorGame.cpp
+++ DinorGame/DinorGame.cpp
@@ -1,165 +1,206 @@
-﻿// DinorGame.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
-
-#include "framework.h"
+﻿#include "framework.h"
 #include "DinorGame.h"
 
 #define MAX_LOADSTRING 100
+#define TIMER_ID 1
+#define TIMER_INTERVAL 50 // 50ms 간격 (약 20fps)
 
-// 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HINSTANCE hInst;                                // 현재 인스턴스
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+// 공룡 도트 그래픽 정의 (1: 픽셀 있음, 0: 없음)
+const int dinoWidth = 20;
+const int dinoHeight = 22;
+const int dinoPixels[dinoHeight][dinoWidth] =
+{
+    {0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0},
+    {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+    {0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1},
+    {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+    {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+    {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+    {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0},
+    {1,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0},
+    {1,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0},
+    {1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0},
+    {1,1,1,0,0,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0},
+    {0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0},
+    {0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0},
+    {0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0},
+    {0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0},
+};
+
+
+
+int dinoPosX = 50;
+int dinoPosY = 100;
+
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: 여기에 코드를 입력합니다.
-
-    // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_DINORGAME, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
+    if (!InitInstance(hInstance, nCmdShow))
     {
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DINORGAME));
-
     MSG msg;
+    // 타이머 설정: 윈도우 생성 후 타이머로 주기적 WM_TIMER 발생
+    SetTimer(NULL, TIMER_ID, TIMER_INTERVAL, NULL);
 
-    // 기본 메시지 루프입니다:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-
-    return (int) msg.wParam;
+    return (int)msg.wParam;
 }
 
-
-
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DINORGAME));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DINORGAME);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DINORGAME));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_DINORGAME);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
 }
 
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+    hInst = hInstance;
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, 640, 480, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+    if (!hWnd)
+    {
+        return FALSE;
+    }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
 
-   return TRUE;
+    // 타이머 설정: 창 핸들을 타이머에 연결
+    SetTimer(hWnd, TIMER_ID, TIMER_INTERVAL, NULL);
+
+    return TRUE;
 }
 
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
+void DrawDino(HDC hdc, int posX, int posY, int pixelSize)
+{
+    for (int y = 0; y < dinoHeight; ++y)
+    {
+        for (int x = 0; x < dinoWidth; ++x)
+        {
+            if (dinoPixels[y][x] == 1)
+            {
+                Rectangle(hdc,
+                    posX + x * pixelSize,
+                    posY + y * pixelSize,
+                    posX + (x + 1) * pixelSize,
+                    posY + (y + 1) * pixelSize);
+            }
+        }
+    }
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    static int dx = 5; // 공룡의 x방향 이동 속도
+
     switch (message)
     {
     case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        switch (wmId)
         {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
         }
-        break;
+    }
+    break;
     case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        // 공룡 도트 그래픽 그리기
+        DrawDino(hdc, dinoPosX, dinoPosY, 10);
+
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+    case WM_TIMER:
+        if (wParam == TIMER_ID)
         {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            EndPaint(hWnd, &ps);
+            // 공룡 좌표 이동 (간단한 애니메이션)
+            dinoPosX += dx;
+
+            // 윈도우 클라이언트 영역 크기 얻기
+            RECT rect;
+            GetClientRect(hWnd, &rect);
+
+            // 화면 밖으로 나가지 않도록 방향 반전
+            if (dinoPosX + dinoWidth * 10 > rect.right || dinoPosX < 0)
+            {
+                dx = -dx;
+            }
+
+            InvalidateRect(hWnd, NULL, TRUE); // 화면 갱신 요청 (WM_PAINT 호출 유발)
         }
         break;
+
     case WM_DESTROY:
+        KillTimer(hWnd, TIMER_ID);
         PostQuitMessage(0);
         break;
+
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
 }
 
-// 정보 대화 상자의 메시지 처리기입니다.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
@@ -167,7 +208,6 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     {
     case WM_INITDIALOG:
         return (INT_PTR)TRUE;
-
     case WM_COMMAND:
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
         {
